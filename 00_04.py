c6811a3e5b8447e62c7de64beacac90f06942a82
netsinweb <nets@mac.com>
2025-10-15 22:20:31 +0900

    Refact: indent space to tab

--- 00_04.py
+++ 00_04.py
@@ -2,5 +2,5 @@
 do_a_flip()
 change_hat(Hats.Purple_Hat)
 while True:
-    if can_harvest():
-        harvest()
+	if can_harvest():
+		harvest()
--- 00_05.py
+++ 00_05.py
@@ -2,6 +2,6 @@
 do_a_flip()
 change_hat(Hats.Gray_Hat)
 while True:
-    if can_harvest():
-        harvest()
-    move(North)
+	if can_harvest():
+		harvest()
+	move(North)
