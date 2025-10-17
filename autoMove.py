9ec02249c34204262af92af72f684b58220eacf2
netsinweb <nets@mac.com>
2025-10-18 01:38:46 +0900

    Update: autoMoveに新しい動きなど追加 - loopSnake() snakeMoveを調整してループするようにコース取り(x方向のサイズが偶数かつx/y方向のサイズがどちらも2以上の時のみ動く) - snakeMove()、loopSnake()とも引数を追加   - x方向のサイズ   - y方向のサイズ   - 矩形の左下のx座標(Default:0)   - 矩形の左下のy座標(Default:0)

--- autoMove.py
+++ autoMove.py
@@ -13,25 +13,58 @@ def pointMove(toX = 0, toY = 0):
 	while get_pos_y() != toY:
 		move(directionY)
 
-def snakeMove(moveSize):
-	maxPos = moveSize - 1
+def snakeMove(sizeX, sizeY, minX = 0, minY = 0):
+	maxX = sizeX - 1
+	maxY = sizeY - 1
 	x = get_pos_x()
 	y = get_pos_y()
-	xEnd = maxPos
-	yEnd = maxPos - maxPos * (1 - moveSize%2)
-	if x == 0 and y == 0:
+	xEnd = maxX
+	yEnd = maxY - maxY * (1 - sizeX%2)
+	if x == minX and y == minY:
 		move(North)
-		return
+		return True
 	if x == xEnd and y == yEnd:
 		pointMove()
-		return
+		return True
 	if x%2 == 1:
-		if y == 0:
+		if y == minY:
 			move(East)
-			return
+			return True
 		move(South)
-		return
-	if y == maxPos:
+		return True
+	if y == maxY:
 		move(East)
-		return
+		return True
 	move(North)
+	return True
+
+def loopSnake(sizeX, sizeY, minX = 0, minY = 0):
+	if sizeX < 2 or sizeY < 2:
+		print("Size too small! (2 or more)")
+		change_hat(Hats.Purple_Hat)
+		return False
+	if (sizeX%2 == 1):
+		print("X size is not an even number!")
+		change_hat(Hats.Purple_Hat)
+		return False
+	maxX = sizeX - 1
+	maxY = sizeY - 1
+	x = get_pos_x()
+	y = get_pos_y()
+	if x == minX and y == minY:
+		move(North)
+		return True
+	if y == minY:
+		move(West)
+		return True
+	if x%2 == 1:
+		if y == (minY + 1) and x < maxX:
+			move(East)
+			return True
+		move(South)
+		return True
+	if y == maxY:
+		move(East)
+		return True
+	move(North)
+	return True
